5025ca9bcb4ecaf2c4dc085b718f54c8d8b02c41
lucaoflaif <lucadivitadev@gmail.com>
2017-12-09 15:34:36 +0100

    first commit

---
+++ coinmarketcapapi/__init__.py
@@ -0,0 +1,88 @@
+import json
+import requests
+import datetime
+import pytz
+
+class CoinMarketCapAPI:
+    '''API Class
+    '''
+    def __init__(self, max_request_per_minute = 9):
+        """: param int max_request_per_minute, if we want a maximum of {parameter}
+             requests per minute, we don't have to make more than 1 request every
+             ({parameter} / 60) seconds, IT'S FREAKIN' EASY M8
+        """
+        self._delay_seconds = 60.0 / max_request_per_minute
+
+        self._cached_api_response = None
+        self._time_cached_api_response = None
+
+        #number of API returned cached data
+        self._n_cache_hits = 0
+
+    API_URL_ROOT = 'https://api.coinmarketcap.com'
+    API_URL_VERSION = 'v1'
+
+
+    def _make_url(self, info_type):
+        url = "{}/{}/{}".format(
+            self.API_URL_ROOT,
+            self.API_URL_VERSION,
+            info_type
+        )
+        return url
+
+    def send_request(self, info_type='ticker', **kwargs):
+        """: param string 'ticker', it's 'ticker' if we want info about coins, 
+            'global' for global market's info.
+        """
+        built_url = self._make_url(info_type)
+        payload = dict(**kwargs)
+
+        self._process_request(built_url, payload)
+    
+    def get_response(self):
+        return self._cached_api_response
+
+    def _cached_data_is_old(self):
+        """: params int minutes_lt, return True if the difference between the time of cached data
+             and now is Greater Than {integer param} in Minutes
+        """
+
+        #If we haven't yet the time of last API call return True
+        #in order to allow the code to do the request
+        if not self._time_cached_api_response:
+            return True
+
+        #Let's check the time now
+        time_now_utc = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+		
+        #Then, calculate the difference between the last time 
+        # we retrieved new data from server and now
+        time_difference = time_now_utc - self._time_cached_api_response
+        
+        seconds_difference = time_difference.total_seconds()
+
+        if self._delay_seconds:
+            return seconds_difference > self._delay_seconds
+
+    def _process_request(self, built_url, payload):
+
+        #if we have no cache's time then call APIs, else use cached data
+        if self._time_cached_api_response is None or self._cached_data_is_old():
+            response = requests.get(built_url, params=payload)
+        else:
+            self._n_cache_hits += 1
+            response = self._cached_api_response
+        self._process_response(response)
+
+    def _process_response(self, response):
+        if isinstance(response, list):
+            #response already converted from json
+            self._cached_api_response = response
+        else:
+            #response in raw format, going to convert it
+            self._cached_api_response = response.json()
+
+        #UTC time must be converted later into local user's time
+        utc_time = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+        self._time_cached_api_response = utc_time
---
+++ tests/cache_test.py
@@ -0,0 +1,54 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+import unittest
+import time, random
+
+import coinmarketcapapi
+
+class CacheTestCase(unittest.TestCase):
+    
+    def test_class_can_cache_api_data_1(self):
+        
+        my_class = coinmarketcapapi.CoinMarketCapAPI()
+        times_of_request = 5
+
+        for x in range (times_of_request):
+            my_class.send_request(convert="EUR")
+    
+
+        self.assertEqual(my_class._n_cache_hits, times_of_request - 1)
+
+    def test_class_can_cache_api_data_2(self):
+            
+        my_class = coinmarketcapapi.CoinMarketCapAPI(max_request_per_minute=30)
+        times_of_request = 5
+
+        for x in range (times_of_request):
+            my_class.send_request(convert="EUR")
+            time.sleep(3)
+
+        self.assertEqual(my_class._n_cache_hits, 0)
+
+    def test_class_can_cache_api_data_3(self):
+        max_requests = 30
+        my_class = coinmarketcapapi.CoinMarketCapAPI(max_request_per_minute=max_requests)
+
+        delay_seconds = 60/max_requests
+        n_of_requests = 5
+        expected_cache_call = 0 
+    
+        for step in range (n_of_requests):
+            my_class.send_request() # Server request
+            delay_time = random.uniform(1,3)
+            print(delay_time)
+            time.sleep(delay_time)
+
+            if (delay_time < delay_seconds) and step < 4: # less than last step, range(5) is 0,1,2,3,4
+                expected_cache_call += 1
+
+        self.assertEqual(my_class._n_cache_hits, expected_cache_call)
+
+
+if __name__ == "__main__":
+    unittest.main()
