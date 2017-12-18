1c4ad995782440856591ae8cc74fad5eab67c130
lucaoflaif <lucadivitadev@gmail.com>
2017-12-18 20:34:42 +0100

    add utils file with dicts filter

---
+++ coinmarketcapapi/utils.py
@@ -0,0 +1,11 @@
+def dicts_filter(dicts_object, field_to_filter, value_of_filter):
+    """This function gets as arguments an array of dicts through the dicts_objects parameter,
+       then it'll return the dicts that have a value value_of_filter as the key field_to_filter.
+    """
+    lambda_query = lambda value: value[field_to_filter] == value_of_filter
+
+    filtered_coin = filter(lambda_query, dicts_object)
+    selected_coins = list(filtered_coin)
+    #if not selected_coin: #Empty list, no coin found
+    #    raise AttributeError('attribute %s not found' % attr)
+    return selected_coins
