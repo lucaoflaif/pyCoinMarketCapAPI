52f123d7210dc3a0d5289dc727bc8efd3c0e235a
lucaoflaif <lucadivitadev@gmail.com>
2017-12-18 20:34:28 +0100

    fix some bugs, add str method

--- coinmarketcapapi/types.py
+++ coinmarketcapapi/types.py
@@ -1,25 +1,27 @@
 import datetime as dt
 
 class Coin:
-    def __init__(self, id, name, rank, price_usd, price_btc, 24h_volume_usd,
-                    market_cap_usd, available_supply, total_supply, max_supply,
-                    percent_change_1h, percent_change_24h, percent_change_7d,
-                    last_updated, price_converted = None, 24h_volume_converted = None, 
-                    market_cap_converted = None):
-        
-        
+    def __init__(self, coin_id, name, symbol, rank, price_usd, price_btc, volume_usd_24h,
+                 market_cap_usd, available_supply, total_supply, max_supply,
+                 percent_change_1h, percent_change_24h, percent_change_7d,
+                 last_updated, price_converted=None, volume_24h_converted=None,
+                 market_cap_converted=None):
+
+        # direct string to int cast isn't possible
         self.rank = int(rank)
-        
+
+        self.symbol = symbol
         self.name = name
-        self.id = id
+        self.coin_id = coin_id
 
         self.price_usd = float(price_usd)
-        self.price_btc = float(price_usd)
+        self.price_btc = float(price_btc)
         self.volume_usd_24h = float(volume_usd_24h)
         self.market_cap_usd = float(market_cap_usd)
         self.available_supply = float(available_supply)
         self.total_supply = float(total_supply)
-        self.max_supply = float(max_supply)
+        if max_supply: #  max supply can be None from APIs
+            self.max_supply = float(max_supply) 
         self.percent_change_1h = float(percent_change_1h)
         self.percent_change_24h = float(percent_change_24h)
         self.percent_change_7d = float(percent_change_7d)
@@ -33,7 +35,12 @@ class Coin:
         if market_cap_converted:
             self.price_converted = float(market_cap_converted)
 
-
+    def __str__(self):
+        return "%(coin_name)s (%(symbol)s)" % {
+            'coin_name': self.name,
+            'symbol': self.symbol
+        }
 
     def _format_last_updated_date(self, timestamp):
+        timestamp = int(timestamp)
         return dt.datetime.fromtimestamp(timestamp)
