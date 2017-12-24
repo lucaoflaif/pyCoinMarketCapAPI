c029ce4f4f8fd6d63d16e8b5f227cd9017611a78
lucaoflaif <d.v.luca99@gmail.com>
2017-12-24 10:36:34 +0100

    prepare package to pypi upload (fix #3)

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(name='pyCoinMarketCapAPI',
+      version='0.1',
+      description="Python support for coimarketcap's APIs with cache and timezones support.",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+
+      ],
+      keywords="cryptocurrency bitcoin api market",
+      url='https://github.com/lucaoflaif/pyCoinMarketCapAPI',
+      author='Luca Di Vita',
+      author_email='d.v.luca99@gmail.com',
+      license='MIT',
+      packages=['coinmarketcapapi'],
+      install_requires=[
+          'os', 'sys',
+          'json',
+          'datetime',
+          'requests',
+          'pytz',
+          'unittest',
+          'time'
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],)
